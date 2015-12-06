b942b2a3df21ad05f8a361b047af60986d665829
Michal Stechly <michal.stechly@gmail.com>
2015-12-06 23:20:05 +0100

    small fix

--- runQueue.py
+++ runQueue.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="A simulation of a queue.")
     parser.add_argument('-T', action="store", dest="T", type=float, help='The overall simulation time', default=0)
     parser.add_argument('-v', action="store", dest="verbose", type=int, help='Verbose', default=0)
-    parser.add_argument('-c', action="store", dest="cockroachSimulation", type=int, help='Is it a simulation for the cockroaches.', default=0)
+    parser.add_argument('-s', action="store", dest="cockroachSimulation", type=int, help='Is it a simulation for the cockroaches.', default=0)
     parser.add_argument('-p', action="store", dest="permutation", type=list, help='Permutation of customers', default=[0])
     parser.add_argument('-f', action="store", dest="fileID", type=int, help='File ID', default=1)
 
@@ -21,12 +21,12 @@ if __name__ == '__main__':
     inputs = parser.parse_args()
     T = inputs.T
     v = inputs.verbose
-    c = inputs.cockroachSimulation
+    s = inputs.cockroachSimulation
     p = inputs.permutation
     f = inputs.fileID
 
     if s==0:
-        q = Sim(f, T, v, c, p)
+        q = Sim(f, T, v, s, p)
         q.run()
 
     if s==1:
