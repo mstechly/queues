97d4f941352642efdc12d15d65a5339c3c476035
Michal Stechly <michal.stechly@gmail.com>
2015-12-06 23:09:28 +0100

    minor fixes

--- runQueue.py
+++ runQueue.py
@@ -5,28 +5,28 @@ import random
 import time
 import datetime
 
-from CockroachLibrary import Cockroach
-from CockroachLibrary import CSO
+from cockroachLibrary import Cockroach
+from cockroachLibrary import CSO
 
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser(description="A simulation of a queue.")
     parser.add_argument('-T', action="store", dest="T", type=float, help='The overall simulation time', default=0)
     parser.add_argument('-v', action="store", dest="verbose", type=int, help='Verbose', default=0)
-    parser.add_argument('-s', action="store", dest="cockroachSimulation", type=int, help='Is it a simulation for the cockroaches.', default=0)
-    parser.add_argument('-p', action="store", dest="permutation", type=list, help='Permutation of Cockroaches', default=[0])
+    parser.add_argument('-c', action="store", dest="cockroachSimulation", type=int, help='Is it a simulation for the cockroaches.', default=0)
+    parser.add_argument('-p', action="store", dest="permutation", type=list, help='Permutation of customers', default=[0])
     parser.add_argument('-f', action="store", dest="fileID", type=int, help='File ID', default=1)
 
 
     inputs = parser.parse_args()
     T = inputs.T
     v = inputs.verbose
-    s = inputs.cockroachSimulation
+    c = inputs.cockroachSimulation
     p = inputs.permutation
     f = inputs.fileID
 
     if s==0:
-        q = Sim(f, T, v, s, p)
+        q = Sim(f, T, v, c, p)
         q.run()
 
     if s==1:
